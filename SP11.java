0a876bd7323c90d2b169bc7307d41d379b10d63b
Sai Krishna Reddy Syamala <saikrishnareddy.gcs@gmail.com>
2018-11-17 22:31:51 -0600

    Added Driver along with the Implementation

---
+++ SP11.java
@@ -0,0 +1,125 @@
+package sxs169430;
+
+import java.util.Random;
+
+public class SP11 {
+    private static final int THRESHOLD = 17;
+    private static Random random = new Random();
+
+    /**
+     * Randomized partition
+     *
+     * @param arr Array to sort
+     * @param p   Starting element
+     * @param r   Last element
+     * @return int, Result of partition
+     */
+    private static int randomizedPartition(Comparable[] arr, int p, int r) {
+        int i = p + random.nextInt(r - p);
+        Comparable t;
+        t = arr[i];
+        arr[i] = arr[r];
+        arr[r] = t;
+        return partition1(arr, p, r);
+    }
+
+    /**
+     * Partition version 1
+     *
+     * @param arr Array to be sorted
+     * @param p   Starting element
+     * @param r   Beginning element
+     * @return int
+     */
+    private static int partition1(Comparable[] arr, int p, int r) {
+        Comparable x = arr[r], t;
+        int i = p - 1;
+        for (int j = p; j < r; j++) {
+            if (arr[j].compareTo(x) <= 0) {
+                i++;
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+        t = arr[i + 1];
+        arr[i + 1] = arr[r];
+        arr[r] = t;
+        return i + 1;
+    }
+
+    /**
+     * Sort the array using Insertion Sort
+     *
+     * @param arr Array to sort
+     * @throws Exception Exception in case incorrect indexes are given
+     */
+    public static void insertionSort(Comparable[] arr) throws Exception {
+        insertionSort(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * Sort array between start and end range using insertion sort
+     *
+     * @param arr   Array to sort
+     * @param start Start range
+     * @param end   End range
+     * @throws Exception Exception in case incorrect indexes are given
+     */
+    public static void insertionSort(Comparable[] arr, int start, int end) throws Exception {
+        if (start >= end || start < 0) {
+            throw new Exception("Incorrect index given for insertion sort");
+        }
+
+        Comparable temp;
+        int i, j;
+        for (i = start + 1; i <= end; i++) {
+            temp = arr[i];
+            j = i - 1;
+            while (j >= start && temp.compareTo(arr[j]) < 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    /**
+     * Select k largest elements from array
+     *
+     * @param arr Input array
+     * @param k   Number of elements
+     */
+    public static void select(Comparable[] arr, int k) throws Exception {
+        select(arr, 0, arr.length, k);
+    }
+
+    /**
+     * Select k largest elements from array
+     *
+     * @param arr Input array
+     * @param p   Start index
+     * @param n   Size of array
+     * @param k   Number of elements to select
+     * @return int
+     * @throws Exception In case insertion sort gets wrong index values
+     */
+    public static int select(Comparable[] arr, int p, int n, int k) throws Exception {
+        int q, left, right;
+        if (n < THRESHOLD) {
+            insertionSort(arr);
+            return 0;
+        } else {
+            q = randomizedPartition(arr, p, p + n - 1);
+            left = q - p;
+            right = n - left - 1;
+            if (right >= k) {
+                return select(arr, q + 1, right, k);
+            } else if (right + 1 == k) {
+                return q;
+            } else {
+                return select(arr, p, left, k - right - 1);
+            }
+        }
+    }
+}
---
+++ SP11Driver.java
@@ -0,0 +1,145 @@
+package sxs169430;
+
+import java.util.Random;
+
+
+public class SP11Driver {
+	public static Random random = new Random();
+    public static int numTrials = 100;
+    public static void main(String[] args) throws Exception {
+        int k = 0,size = 0;
+        int choice = 4;
+        if(args.length > 0) { size = Integer.parseInt(args[0]); }
+        if(args.length > 1) { k = Integer.parseInt(args[1]);}
+        if(args.length > 2) { choice = Integer.parseInt(args[2]); }
+        Comparable[] arr = new Comparable[size];
+        for(int i=0; i<size; i++) {
+            arr[i] = i+1;
+        }
+        Timer timer = new Timer();
+        switch(choice) {
+        	// for testing
+            case 1:
+                Shuffle.shuffle(arr);
+                for(int i=0;i<k;i++)
+                	System.out.print(arr[i]+" ");
+                System.out.println();
+                SP11.select(arr,k);
+                for(int i=0;i<k;i++)
+                	System.out.print(arr[i]+" ");
+                break;
+            case 2:
+            	for(int i=0; i<numTrials; i++) {
+                    Shuffle.shuffle(arr);
+                    SP11.select(arr,k);
+                }
+            	break;
+            default:
+                for(int i=0; i<numTrials; i++) {
+                    Shuffle.shuffle(arr);
+                }
+                break;
+
+        }
+        timer.end();
+        timer.scale(numTrials);
+
+        System.out.println("Choice: " + choice + "\n" + timer);
+    }
+    
+    public static class Shuffle {
+
+        public static void shuffle(int[] arr) {
+            shuffle(arr, 0, arr.length-1);
+        }
+
+        public static<T> void shuffle(T[] arr) {
+            shuffle(arr, 0, arr.length-1);
+        }
+
+        public static void shuffle(int[] arr, int from, int to) {
+            int n = to - from  + 1;
+            for(int i=1; i<n; i++) {
+                int j = random.nextInt(i);
+                swap(arr, i+from, j+from);
+            }
+        }
+
+        public static<T> void shuffle(T[] arr, int from, int to) {
+            int n = to - from  + 1;
+            Random random = new Random();
+            for(int i=1; i<n; i++) {
+                int j = random.nextInt(i);
+                swap(arr, i+from, j+from);
+            }
+        }
+
+        static void swap(int[] arr, int x, int y) {
+            int tmp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = tmp;
+        }
+
+        static<T> void swap(T[] arr, int x, int y) {
+            T tmp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = tmp;
+        }
+
+        public static<T> void printArray(T[] arr, String message) {
+            printArray(arr, 0, arr.length-1, message);
+        }
+
+        public static<T> void printArray(T[] arr, int from, int to, String message) {
+            System.out.print(message);
+            for(int i=from; i<=to; i++) {
+                System.out.print(" " + arr[i]);
+            }
+            System.out.println();
+        }
+    }
+    
+    /** Timer class for roughly calculating running time of programs
+     *  @author rbk
+     *  Usage:  Timer timer = new Timer();
+     *          timer.start();
+     *          timer.end();
+     *          System.out.println(timer);  // output statistics
+     */
+
+    public static class Timer {
+        long startTime, endTime, elapsedTime, memAvailable, memUsed;
+        boolean ready;
+
+        public Timer() {
+            startTime = System.currentTimeMillis();
+            ready = false;
+        }
+
+        public void start() {
+            startTime = System.currentTimeMillis();
+            ready = false;
+        }
+
+        public Timer end() {
+            endTime = System.currentTimeMillis();
+            elapsedTime = endTime-startTime;
+            memAvailable = Runtime.getRuntime().totalMemory();
+            memUsed = memAvailable - Runtime.getRuntime().freeMemory();
+            ready = true;
+            return this;
+        }
+
+        public long duration() { if(!ready) { end(); }  return elapsedTime; }
+
+        public long memory()   { if(!ready) { end(); }  return memUsed; }
+
+        public void scale(int num) { elapsedTime /= num; }
+
+        public String toString() {
+            if(!ready) { end(); }
+            return "Time: " + elapsedTime + " msec.\n" + "Memory: " + (memUsed/1048576) + " MB / " + (memAvailable/1048576) + " MB.";
+        }
+    }
+    
+}
